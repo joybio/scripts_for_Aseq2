f4c52e04254146a71eae41ad8b42847a87725cfe
Junbo Yang <41945265+joybio@users.noreply.github.com>
2020-07-07 10:38:21 +0800

    Update feature_annotation.py
    。。。

--- feature_annotation/feature_annotation.py
+++ feature_annotation/feature_annotation.py
@@ -33,6 +33,7 @@ parser.add_option('-d','--distribution',
                 help='distribution annotation file')
 (options,args) = parser.parse_args()
 
+
 #generating row_feature_annotate.gtf
 '''
 iterate through gtf file and return a gtf.bed that contains for every 
@@ -47,16 +48,21 @@ genomic position of exon, 5' UTR, 3' UTR, gene
 with open("row_feature_annotate.gtf.bed","w") as out:
 	with open(options.gtf,'r') as data:
 		for i in data:
-        		if i.startswith("#"):
-                		pass
-        		else:
-                		i=i.strip().split('\t')
-				if i[2] == 'transcript':
-					pass
-				elif i[2] == 'CDS':
+			if i.startswith("#"):
+				pass
+			else:
+				i=i.strip().split('\t')
+				if len(i) < 3:
 					pass
 				else:
-                			out.write(i[0] + '\t' + i[3] + '\t' + i[4] +'\t' + i[2] + "\t" + i[6] + '\n')
+					if i[2] == 'transcript':
+						pass
+					elif i[2] == 'CDS':
+						pass
+					elif i[2] == "mRNA":
+						pass
+					else:
+						out.write(i[0] + '\t' + i[3] + '\t' + i[4] +'\t' + i[2] + "\t" + i[6] + '\n')
 		data.close()
 	out.close()
 
@@ -67,47 +73,47 @@ out = open('temp_step2_feature_annotate.gtf.bed','w')
 #We divided transcripts into five segments: 5'UTRs, start codons (200-nucleotide window centered on the start codon), coding sequences (CDSs), stop codons (200-nucleotide window centered on the stop codon), and 3'UTRs.
 for i in data:
 	TYPE = i
-        i = i.strip().split("\t")
-        L = i[0] + "\t" + i[1] + "\t" + i[2] + '\t' + i[3] + '\t' + i[4] + '\t' + i[5] + '\t' + i[7]
-        l = L.strip()
-        if re.search("start_codon",TYPE):
-                start = int(i[4])
-                stop = int(i[5])
-                left_border = int(i[1])
-                right_border = int(i[2])
-                if i[7] == "+":
-                        if (start - left_border >= 100) and (right_border - stop >= 100):
-                                out.write(l + "\t"  + "start_codon" + "\n")
-                        elif (start - left_border) > (right_border - stop):
-                                out.write(l + "\t"  + "five_prime_utr" + "\n")
-                        else:
-                                out.write(l + "\t"  + "exon" + "\n")
-                else:
-                        if (start - left_border <= -100) and (right_border - stop <= -100):
-                                out.write(l + "\t"  + "start_codon" + "\n")
-                        elif (start - left_border) < (right_border - stop):
-                                out.write(l + "\t"  + "exon" + "\n")
-                        else:
-                                out.write(l + "\t"  + "five_prime_utr" + "\n")
-        elif re.search("stop_codon",TYPE):
-                start = int(i[4])
-                stop = int(i[5])
-                left_border = int(i[1])
-                right_border = int(i[2])                
-                if i[7] == "+":
-                        if (start - left_border >= 100) and (right_border - stop >= 100):
-                                out.write(l + "\t"  + "stop_codon" + "\n")
-                        elif (start - left_border) > (right_border - stop):
-                                out.write(l + "\t"  + "three_prime_utr" + "\n")
-                        else:
-                                out.write(l + "\t"  + "exon" + "\n")
-                else:
-                        if (start - left_border <= -100) and (right_border - stop <= -100):
-                                out.write(l + "\t"  + "stop_codon" + "\n")
-                        elif (start - left_border) < (right_border - stop):
-                                out.write(l + "\t"  + "exon" + "\n")
-                        else:
-                                out.write(l + "\t"  + "three_prime_utr" + "\n")
+	i = i.strip().split("\t")
+	L = i[0] + "\t" + i[1] + "\t" + i[2] + '\t' + i[3] + '\t' + i[4] + '\t' + i[5] + '\t' + i[7]
+	l = L.strip()
+	if re.search("start_codon",TYPE):
+		start = int(i[4])
+		stop = int(i[5])
+		left_border = int(i[1])
+		right_border = int(i[2])
+		if i[7] == "+":
+			if (start - left_border >= 100) and (right_border - stop >= 100):
+				out.write(l + "\t"  + "start_codon" + "\n")
+			elif (start - left_border) > (right_border - stop):
+				out.write(l + "\t"  + "five_prime_utr" + "\n")
+			else:
+				out.write(l + "\t"  + "exon" + "\n")
+		else:
+			if (start - left_border <= -100) and (right_border - stop <= -100):
+				out.write(l + "\t"  + "start_codon" + "\n")
+			elif (start - left_border) < (right_border - stop):
+				out.write(l + "\t"  + "exon" + "\n")
+			else:
+				out.write(l + "\t"  + "five_prime_utr" + "\n")
+	elif re.search("stop_codon",TYPE):
+		start = int(i[4])
+		stop = int(i[5])
+		left_border = int(i[1])
+		right_border = int(i[2])		
+		if i[7] == "+":
+			if (start - left_border >= 100) and (right_border - stop >= 100):
+				out.write(l + "\t"  + "stop_codon" + "\n")
+			elif (start - left_border) > (right_border - stop):
+				out.write(l + "\t"  + "three_prime_utr" + "\n")
+			else:
+				out.write(l + "\t"  + "exon" + "\n")
+		else:
+			if (start - left_border <= -100) and (right_border - stop <= -100):
+				out.write(l + "\t"  + "stop_codon" + "\n")
+			elif (start - left_border) < (right_border - stop):
+				out.write(l + "\t"  + "exon" + "\n")
+			else:
+				out.write(l + "\t"  + "three_prime_utr" + "\n")
 	else:
 		out.write(l + "\t"  + i[6] + "\n")
 data.close()
@@ -115,42 +121,42 @@ out.close()
 
 temp1_dict = {}
 with open('temp_step2_feature_annotate.gtf.bed','r') as f:
-        for i in f:
-                i = i.strip().split('\t')
-                key = str(str(i[0])+','+str(i[1])+','+str(i[2]))
-                if key in temp1_dict.keys():
-                        temp1_dict[key] += '\t' + i[7]
-                else:
-                        temp1_dict[key] = i[7]
-        with open('temp_step3_feature_annotate.gtf.bed','w') as fh:
-                for i in temp1_dict.keys():
-                        fh.write(i + '\t' + temp1_dict[i] + '\n')
-        fh.close()
+	for i in f:
+		i = i.strip().split('\t')
+		key = str(str(i[0])+','+str(i[1])+','+str(i[2]))
+		if key in temp1_dict.keys():
+			temp1_dict[key] += '\t' + i[7]
+		else:
+			temp1_dict[key] = i[7]
+	with open('temp_step3_feature_annotate.gtf.bed','w') as fh:
+		for i in temp1_dict.keys():
+			fh.write(i + '\t' + temp1_dict[i] + '\n')
+	fh.close()
 f.close()
 
 def prior(TYPE):
-        if re.search("start_codon",TYPE):
-                return("Start_Codon")
-        elif re.search("stop_codon",TYPE):
-                return("Stop_Codon")
-        elif re.search("three_prime_utr",TYPE):
-                return("3'UTR")
-        elif re.search("five_prime_utr",TYPE):
-                return("5'UTR")
-        elif re.search("exon",TYPE):
-                return("CDS")
-        elif re.search("gene",TYPE):
-                return("intron")
+	if re.search("start_codon",TYPE):
+		return("Start_Codon")
+	elif re.search("stop_codon",TYPE):
+		return("Stop_Codon")
+	elif re.search("three_prime",TYPE):
+		return("3'UTR")
+	elif re.search("five_prime",TYPE):
+		return("5'UTR")
+	elif re.search("exon",TYPE):
+		return("CDS")
+	elif re.search("gene",TYPE):
+		return("intron")
 
 data = open(options.bed,'r')
 out = open(options.out,'w')
 for i in data:
-        i = i.strip().split("\t")
-        key=str(i[0]) + ',' + str(i[1]) + ',' + str(i[2])
-        if key in temp1_dict.keys():
-                out.write(key + "\t"  + prior(temp1_dict[key]) + "\n")
-        else:
-                out.write(key + "\t"  + 'intergenic' + '\n')
+	i = i.strip().split("\t")
+	key=str(i[0]) + ',' + str(i[1]) + ',' + str(i[2])
+	if key in temp1_dict.keys():
+		out.write(key + "\t"  + prior(temp1_dict[key]) + "\n")
+	else:
+		out.write(key + "\t"  + 'intergenic' + '\n')
 data.close()
 out.close()
 
@@ -167,17 +173,17 @@ with open(options.out,'r') as f:
 		TYPE = i.strip().split('\t')
 		total += 1
 		if re.search("Start_Codon",TYPE[1]):
-                	start += 1
-	        elif re.search("Stop_Codon",TYPE[1]):
-        	        stop += 1
-	        elif re.search("3",TYPE[1]):
-        	        UTR3 += 1
-	        elif re.search("5",TYPE[1]):
-        	        UTR5 += 1
+			start += 1
+		elif re.search("Stop_Codon",TYPE[1]):
+			stop += 1
+		elif re.search("3",TYPE[1]):
+			UTR3 += 1
+		elif re.search("5",TYPE[1]):
+			UTR5 += 1
 		elif re.search("CDS",TYPE[1]):
-                        CDS += 1
-	        elif re.search("intron",TYPE[1]):
-        	        intron += 1
+			CDS += 1
+		elif re.search("intron",TYPE[1]):
+			intron += 1
 		elif re.search("intergenic",TYPE[1]):
 			intergenic += 1
 f.close()
@@ -193,5 +199,4 @@ with open(options.dis,'w') as data:
 		+ "intergenic" + '\t' + str(intergenic) + '\t' + str(round(intergenic,4)/round(total,4)) + "\n")
 data.close()
 
-#os.system("rm row_feature_annotate.gtf.bed temp_step*")
-
+os.system("rm row_feature_annotate.gtf.bed temp_step*")
